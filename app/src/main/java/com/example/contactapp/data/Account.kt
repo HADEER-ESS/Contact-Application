3d5b322bdecdae9c9d177b0d9c22f7276b826740
hadeer <hadeeressam21@gmail.com>
2024-07-26 00:07:38 +0300

    create ACCOUNT data class for account content.

---
+++ app/src/main/java/com/example/contactapp/data/Account.kt
@@ -0,0 +1,10 @@
+package com.example.contactapp.data
+
+import android.text.Editable
+
+data class Account(
+    var id: Int,
+    var name:String,
+    var phone: String,
+    var description: String?,
+)
